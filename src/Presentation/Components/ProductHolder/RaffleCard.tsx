0ce8931a68cd461fc429d5d8ed6dcd5714e11472
Diego Vieira <dvsdiegovieira@gmail.com>
2024-06-27 16:27:25 -0300

    refact: refact code on raffle card

--- src/Presentation/Components/ProductHolder/RaffleCard.tsx
+++ src/Presentation/Components/ProductHolder/RaffleCard.tsx
@@ -12,26 +12,23 @@ import TruncatedText from '../Inputs/TruncatedText';
 import RaffleModel from '../../../main/models/RafflesModel';
 import { useShopCar } from '../../../main/hooks/useShopCar';
 import RaffleButton from '../Inputs/RaffleButton';
+import dayjs from 'dayjs';
 
 interface RaffleCardInterface {
     raffle: Raffle;
 }
 
 export default function RaffleCard({ raffle }: RaffleCardInterface) {
-    const [isHover, setIsHover] = useState(false)
-    const { onChangeValue } = RaffleModel()
-    const { setRaffleId } = useShopCar()
+    const { handleSetRaffleId } = useShopCar()
     const history = useNavigate()
     const goToRafflePage = () => {
-        setRaffleId(raffle.id)
-        history(`/home/shop/${raffle.title.split(' ').join('-').toLocaleLowerCase()}`)
+        handleSetRaffleId(raffle.id)
+        history(`/rifa/${raffle.title.split(' ').join('-').toLocaleLowerCase()}`)
     }
- 
     return (
-        <Flex direction={'column'} textAlign='left' w={'18rem'} className='raffle-card' position='relative'
-             >
-            <Text cursor='pointer' onClick={() => goToRafflePage()}  fontSize='2rem' fontWeight='bold' textOverflow={'ellipsis'}
-             textAlign='center'>{raffle.title}</Text>
+        <Flex direction={'column'} textAlign='left' w={'18rem'} className='raffle-card' position='relative'>
+            <Text cursor='pointer' onClick={() => goToRafflePage()} fontSize='2rem' fontWeight='bold' textOverflow={'ellipsis'}
+                textAlign='center'>{raffle.title}</Text>
             <Text cursor='pointer' fontSize='1rem' mb='0.5rem'
                 fontWeight='regular'>DISPONÍVEIS: {Number(raffle.free)}</Text>
             <Image w={'100%'} height='19rem' src={raffle.imageUrl} />
@@ -40,5 +37,6 @@ export default function RaffleCard({ raffle }: RaffleCardInterface) {
             </Box>
             <RaffleButton full={false} width='12.5rem' labelName="Números" onClick={() => goToRafflePage()} />
         </Flex>
+
     );
 }
